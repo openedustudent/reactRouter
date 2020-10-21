2ef74a1f773d8becde230d4c1d4c953ef66cc235
openedustudent <72771586+openedustudent@users.noreply.github.com>
2020-10-21 03:28:04 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ const x = require('express');
 const app = x();
 app
 .use(x.static('./build'))
-.get('*', r => r.res.send('OK'))
+.get('*', r => r.res.sendFile('public/index.html', { root: '.' }))
 .listen(process.env.PORT);
